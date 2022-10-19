bd6f84d0fb67bc1a7e219756b4f658123c2a3247
donkey_kong <donghui8266@gmail.com>
2022-10-19 13:56:55 +0900

    react-router-dom 설치 라우팅 구현

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import req from "./apis/requests";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-import Row from "./components/Row";
+import DetailPage from "./pages/DetailPage";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        id=" NO"
-        fetchUrl={req.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" id="TN" fetchUrl={req.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={req.fetchTopRated} />
-      <Row title="Action Movies" id="AM" fetchUrl={req.fetchActionMovies} />
-      <Row title="Comedy Movies" id="CM" fetchUrl={req.fetchComedyMovies} />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
